9fc7ddc8804015e041d7107c84d1010f150c3b98
test <test@test.com>
2023-02-21 16:18:59 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,5 @@
-use std::thread;
-use std::time::Duration;
+use std::sync::mpsc;
 
 fn main() {
-    let v = vec![1, 2, 3];
-    let v2 = vec![4, 5, 6];
-
-    // move で所有権を強制的に奪わせる
-    let handle = thread::spawn(move || {
-        println!("メインスレッドのベクタ {:?}", v);
-    });
-
-    drop(v2);
-
-    handle.join().unwrap();
+    let (tx, rx) = mpsc::channel();
 }
