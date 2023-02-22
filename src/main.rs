17226ba13a545b9192dc018c8609d907e9cd3472
test <test@test.com>
2023-02-22 12:33:13 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,44 @@
 use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
+    let (sender, receiver) = mpsc::channel();
+
+    let sender_cloned = mpsc::Sender::clone(&sender);
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("ハイ"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            sender_cloned.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    
+    
+   thread::spawn(move || {
+        let vals = vec![
+            String::from("さらなる"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            sender.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    
+    
+    // recv() を呼ばなくても待機して取得してくれる
+    for received in receiver {
+        println!("取得： {}", received);
+    }
 }
+
